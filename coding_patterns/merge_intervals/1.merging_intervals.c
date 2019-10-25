890c96cfbf8413bf5860fc420c51f2f4f0f6142b
nullbyte91 <nullbyte.in@gmail.com>
2019-10-25 10:33:16 +0530

    merge interval tech added

---
+++ coding_patterns/merge_intervals/1.merging_intervals.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Intervals{
+    int start, end;  
+};
+
+struct Stack{
+    struct Interval *elt;
+    int top;
+};
+
+static int cmprow(const void *a, const void *b)
+{
+    const int * const ia = a, * const ib = b;
+    return ia[0] < ib[0] ? -1 : ia[0] > ib[0];
+}
+void
+push(struct Stack *s, int *arr){
+    struct Stack *s = (struct Stack*)malloc(sizeof(struct Stack));
+    s->elt->start = arr->start;
+}
+
+void 
+mergeIntervals(struct Intervals arr[], int n){
+    printf("Before swap:\n");
+    for (int i=0; i<n; i++){
+        printf("S:%d ", arr[i].start);
+        printf("S:%d\n", arr[i].end);
+    } 
+    qsort(arr, n, sizeof *arr, cmprow);
+    printf("After swap:\n");
+    for (int i=0; i<n; i++){
+        printf("S:%d ", arr[i].start);
+        printf("S:%d\n", arr[i].end);
+    }
+    struct Stack *s = (struct Stack*)malloc(sizeof(struct Stack));
+    s->top = -1;
+}
+
+int 
+main(int argc, char** argcv){
+    struct Intervals arr[] =  { {6,8}, {1,9}, {2,4}, {4,7} };
+    int n = sizeof(arr)/sizeof(arr[0]);
+    mergeIntervals(arr, n);
+    return 0;
+}
